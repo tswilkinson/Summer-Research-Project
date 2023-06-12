62cc5f19643b9f298cc2d40ea1be3f13d6751a5c
Tom Wilkinson <email@tswilkinson.me>
2023-06-12 12:36:22 +0100

    not working

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from sklearn.linear_model import LogisticRegression
 
 cor_size = 0.2
 PS_bound = 0.1
+num_post = 2000
+cred = 0.95
 
 d=100
 n=500
 sig_n = 1.0
-ATE_true = 1.0
 
 def prop_score(x):
     if isinstance(x,(list,np.ndarray)):
@@ -40,6 +41,7 @@ prop = np.asarray([max(min(prop[i],1.0-PS_bound),PS_bound) for i in range(n)])
 k = GPy.kern.RBF(d+1,active_dims=list(range(d+1)),name='rbf',ARD=True)
 m = GPy.models.GPRegression(Z,Y,k)
 m.optimize()
+sigma_sq = m.Gaussian_noise.variance
 
 Z_BB = np.hstack((np.repeat(X,2,axis=0),np.asarray([0.0,1.0]*n).reshape(2*n,1)))
 data_filter = [False]*(2*n)
@@ -58,5 +60,46 @@ M=np.mean(abs(PS_weight[data_filter]))
 cor_var = (cor_size**2)*m.rbf.variance/((M**2)*n)
 
 PriorCov_BB = k.K(Z_BB,Z_BB) + cor_var*np.matmul(PS_weight,PS_weight.T)
-(PriorCov_eigvals,PriorCov_eigvecs) = np.linalg.eigh(PriorCov_BB)
+PriorCov_observed = PriorCov_BB[data_filter,:][:,data_filter]
+(PriorCov_eigvals,PriorCov_eigvecs) = np.linalg.eigh(PriorCov_observed)
+print(PriorCov_BB[1:10,1:10])
 print(min(PriorCov_eigvals),max(PriorCov_eigvals))
+print()
+
+tmp = PriorCov_BB[:,data_filter]
+K_Lm_n = np.zeros((n,n))
+K_Lm_Lm = np.zeros((n,n))
+for i in range(n):
+    K_Lm_n[i,:] = tmp[2*i+1,:] - tmp[2*i,:]
+    for j in range(i+1):
+        K_Lm_Lm[i,j] = PriorCov_BB[2*i+1,2*j+1]-PriorCov_BB[2*i+1,2*j]-PriorCov_BB[2*i,2*j+1]+PriorCov_BB[2*i,2*j]
+        K_Lm_Lm[j,i] = K_Lm_Lm[i,j]
+
+func = lambda l: 1/(l+sigma_sq)
+the_diag = np.diag(func(PriorCov_eigvals))
+mean_left = np.matmul(K_Lm_n,the_diag)
+mean_right = np.matmul(PriorCov_eigvecs.T,Y)
+
+for m in range(50,n+1,50):
+    print("m = ",m)
+    mean_left_m = mean_left[:,n-m:]
+    mean_right_m = mean_right[n-m:]
+    K_Lm_m = K_Lm_n[:,n-m:]
+
+    meanLm = np.matmul(mean_left_m,mean_right_m)
+    covLm = K_Lm_Lm - np.matmul(mean_left_m,K_Lm_m.T)
+
+    Chol_Lm = np.linalg.cholesky(covLm)
+    ATE = np.zeros(num_post)
+    for i in range(num_post):
+        DP_weights = np.random.exponential(1,n)
+        DP_weights = DP_weights/sum(DP_weights)
+        GP_draw = meanLm + np.matmul(Chol_Lm,np.random.normal(0,1,(n,1)))
+        ATE[i] = np.dot(DP_weights,GP_draw)
+
+    print("mean = ",np.mean(meanLm))
+    low = np.quantile(ATE,(1-cred)/2)
+    up  = np.quantile(ATE,(1+cred)/2)
+    print("up = ",up)
+    print("low = ",low)
+    print()
